571fdf24deea9cf6c395bb00f57297b3c83b555a
Robert Allmond <ballmond@gmail.com>
2020-12-31 13:02:06 -0500

    added cms strapi

---
+++ packages/cms-strapi/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '9590924e36e3db307ae1c2feea322aa3'),
+    },
+  },
+});
---
+++ packages/cms-strapi/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '6f2695cf-d986-405a-ad0a-def8cb9f4cbc'
+};
